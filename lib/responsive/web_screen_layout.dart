2e0842590ea6802155eeb006d67cc35d4c27e436
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2023-11-08 16:04:36 -0500

    WebScreenLayout-Main Screen AppBar

--- lib/responsive/web_screen_layout.dart
+++ lib/responsive/web_screen_layout.dart
@@ -1,11 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../colors.dart';
 
 class WebScreenLayout extends StatelessWidget {
   const WebScreenLayout({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: backgroundColor,
+        elevation: 0,
+        actions: [
+          TextButton(
+            onPressed: () {},
+            child: const Text(
+              "Gmail",
+              style: TextStyle(
+                color: primaryColor,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ),
+          TextButton(
+            onPressed: () {},
+            child: const Text(
+              "Images",
+              style: TextStyle(
+                color: primaryColor,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: SvgPicture.asset(
+              'assets/images/more-apps.svg',
+              colorFilter: const ColorFilter.mode(primaryColor, BlendMode.srcIn),
+            ),
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10.0).copyWith(right: 10),
+            child: MaterialButton(
+              onPressed: () {},
+              color: const Color(0xff1A73EB),
+              child: const Text(
+                "Sign in",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          )
+        ],
+      ),
       body: Center(
         child: Text("Hello from Web"),
       ),
