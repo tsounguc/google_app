33ef9cbf64457a1d5462f9386c32639a92efa5aa
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2023-11-08 10:22:46 -0500

    create responsive layout

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:google_app/responsive/mobile_screen_layout.dart';
+import 'package:google_app/responsive/responsive_layout_screen.dart';
+import 'package:google_app/responsive/web_screen_layout.dart';
+
+import 'colors.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Google Clone',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: backgroundColor,
+      ),
+      home: const ResponsiveLayoutScreen(
+        mobileScreenLayout: MobileScreenLayout(),
+        webScreenLayout: WebScreenLayout(),
+      ),
+    );
+  }
+}
